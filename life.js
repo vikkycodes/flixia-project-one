8a01db87cef1cb4b9002c88aca4a1b7b9c0a4eff
vikkycodes <okorovictor185@gmail.com>
2022-10-29 19:04:53 +0100

    worked on the menu bar and carousel

--- life.js
+++ life.js
@@ -1,3 +1,28 @@
+<script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>;
+new Glide(".glide", {
+  type: "carousel",
+  perView: 4,
+  focusAt: "center",
+  gap: 20,
+  breakpoints: {
+    1200: {
+      perView: 3,
+    },
+    900: {
+      perView: 2,
+    },
+    650: {
+      focusAt: "center",
+      gap: 5,
+      perView: 3,
+    },
+    375: {
+      perView: 1,
+      gap: 5,
+    },
+  },
+}).mount();
+
 var a;
 function show_hide() {
   if (a == 1) {
